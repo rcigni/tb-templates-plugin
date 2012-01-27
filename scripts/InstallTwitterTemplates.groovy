a4dec14bbe205dcc5cb93094ea57f96def9d10ef
Raffaele Cigni <rcigni@byte-code.com>
2012-01-27 02:54:57 +0100

    install ant script

--- scripts/InstallTwitterTemplates.groovy
+++ scripts/InstallTwitterTemplates.groovy
@@ -2,11 +2,13 @@ includeTargets << grailsScript("_GrailsInit")
 
 target(main: "The description of the script goes here!") {
 
-    ant.echo(message: 'Copy templates to src/templates')
+    ant.echo(message: 'Copying templates to src/templates')
 
     ant.copy(todir: 'src/templates') {
         fileset(dir: "${pluginLocation}/src/templates")
     }
+
+    ant.echo(message: 'Done successful!')
 
 }
 
