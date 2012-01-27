5af13dd5bbdd10d51afce0797db72a0e6f56552f
Raffaele Cigni <rcigni@byte-code.com>
2012-01-27 03:16:01 +0100

    declare dependency to twitter-bootstap plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -30,7 +30,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
+              ":release:1.1.5") {
             export = false
         }
     }
